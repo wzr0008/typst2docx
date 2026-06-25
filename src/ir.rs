5c6ce6e434038b54281f52b804d2d0f181cd218f
aubur <wzr0008@auburn.edu>
2026-06-25 16:41:28 -0700

    add ir

--- src/ir.rs
+++ src/ir.rs
@@ -17,5 +17,22 @@ pub struct Document{
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Block{
-
+    Heading{level: u8, inlines: Vec<Inline>},
+    Paragraph(Vec<Inline>),
+    List(List),
+    Table(Table),
+    DisplayMath(String),
+    CodeBlock(String),
 }
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct List{
+    pub ordered: bool,
+    pub items: Vec<ListItem>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ListItem{
+    pub blocks: Vec<Block>,
+}
+
